bec069c96dc40d2ba4ce3c071e39604172d6ab03
SamantaMelissa <61596646+SamantaMelissa@users.noreply.github.com>
2023-08-28 21:10:04 -0300

    Subindo atv Encontro remoto 01

---
+++ Biblioteca/Repositories/LivroRepository.cs
@@ -0,0 +1,21 @@
+﻿using Biblioteca.Contexts;
+using Biblioteca.Models;
+
+namespace Biblioteca.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly BibliotecaContext _context;
+
+        public LivroRepository(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar() { 
+            return _context.Livros.ToList();
+        
+        }
+
+    }
+}
